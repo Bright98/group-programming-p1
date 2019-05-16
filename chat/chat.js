1ac33b83a0763b6054feb5e938837917c5544553
Bright98 <hshayan@H.shayan>
2019-05-16 14:46:46 +0430

    solved username problem

--- chat/chat.js
+++ chat/chat.js
@@ -1,8 +1,44 @@
+// //server
+
+// var express = require('express');
+// var app = express();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+
+// app.use(express.static(__dirname));
+// app.get('/', function(req, res)
+// {
+//   res.sendFile(__dirname + '/chat.html');
+// });
+
+// users = [];
+
+// io.on('connection', function(socket)
+// {
+
+//   socket.on('username', function(name)
+//   {
+//     users.push(name);
+//     io.emit('set username', {usr : name});
+//   });
+
+//   socket.on('chat message', function(msg)
+//   {
+//     io.emit('message', msg);
+//   });
+
+// });
+
+// http.listen(3000, function()
+// {
+//   console.log('listening on *:3000');
+// });
 var express=require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var users=[];
 app.use(express.static(__dirname));
 app.get('/', function(req, res)
 {
@@ -17,6 +53,7 @@ io.on('connection', function(socket)
   });
   socket.on('username', function(name)
   {
+    users.push(name);
     io.emit('username', name);
   });
 
--- chat/script.js
+++ chat/script.js
@@ -1,25 +1,62 @@
+// //client
+// var msg;
+// msg = $('#text').val('');
+// $(function()
+// {
+//     var socket = io();
+//     $('#username').submit(function(event)
+//     {
+//         event.preventDefault();
+//         socket.emit('username', $('#username_input').val());
+//         $('#login_page').fadeOut('fast');
+//         $("#main_container").show();
+//         return false;
+//     });
+//     $('#form').submit(function(event)
+//     {
+//         event.preventDefault();
+//         socket.emit('chat message', $('#text').val());
+//         $('#text').val('');
+//         return false;
+//     });
+//     var user;
+//     socket.on('set username', function(name)
+//     {
+//         user = name.usr;
+//         $("#usr").append($('<li>').text(user));
+//     });
+//     //socket.emit('chat message', {message : msg , _user : user});
+//     socket.on('message', function(data)
+//     {
+//             //$("#usr").append($('<li>').text(data._user));
+//             $("#message").append($('<li>').text(msg));
+//     });
+// });
+
 $(function()
 {
     var socket = io();
     var username;
+    var msg;
     $('#username').submit(function(event)
     {
         event.preventDefault();
-        socket.emit('username', $("username_input").val());
         username = $('#username_input').val();
+        socket.emit('username', username);
         $('#login_page').fadeOut('fast');
         $("#main_container").show();
     });
     $('#form').submit(function(event)
     {
         event.preventDefault();
-        socket.emit('chat message', $('#text').val());
+        msg = $('#text').val();
+        socket.emit('chat message', {message : msg, _user : username});
         $('#text').val('');
         return false;
     });
 
-    socket.on('chat message', function(msg)
+    socket.on('chat message', function(data)
     {
-        $("#message").append($('<li>').text(username + ":    " + msg));
+        $("#message").append($('<li>').text(data._user + ":     " + data.message));
     });
 });
